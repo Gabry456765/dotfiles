97376f5f51fc8d7dc4083352cab504125b5726b7
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-18 11:47:31 +0200

    hypr: nvim: Tweak snippets
    nothing much ig
    
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- configs/nvim/lua/snippets/shell.lua
+++ configs/nvim/lua/snippets/shell.lua
@@ -6,8 +6,7 @@ local i = ls.insert_node
 ls.add_snippets("sh", {
   s("if", {
     t("if [ "), i(1), t(" ]; then"),
-    t({ "", "" }),
-    i(2),
+    t({ "", "  " }), i(2),
     t({ "", "fi" }),
   }),
 })
@@ -15,11 +14,9 @@ ls.add_snippets("sh", {
 ls.add_snippets("sh", {
   s("elif", {
     t("if [ "), i(1), t(" ]; then"),
-    t({ "", "" }),
-    i(2),
+    t({ "", "  " }), i(2), 
     t({ "", "elif [ " }), i(3), t(" ]; then"),
-    t({ "", "" }),
-    i(4),
+    t({ "", "  " }), i(4),  
     t({ "", "fi" }),
   }),
 })
