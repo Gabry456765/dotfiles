596e9e34c39fe78dc39367b2c466a7cdb8007f51
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-18 11:29:38 +0200

    hypr: nvim: Customize lualine
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -186,12 +186,29 @@ require("noice").setup({
 -- LuaLine
 require("lualine").setup({
   extensions = {'nvim-tree'},
-  options = { 
+  options = {
+    icons_enabled = true,
     theme = "catppuccin" 
   },
   sections = {
-    lualine_c = { "filename" },
-    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {},
+  },
+  sections = {
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
   },
 })
 
